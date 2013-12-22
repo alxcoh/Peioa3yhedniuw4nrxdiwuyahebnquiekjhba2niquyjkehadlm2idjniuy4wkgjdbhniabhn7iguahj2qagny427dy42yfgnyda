ae4d7335a8a481b0ce18153bca37c9c1bc94fa08
71619997a <marksg07@gmail.com>
2013-12-22 17:57:10 -0500

    dpenis

--- EclipseShitzProjects/CommonFunctions/src/root/nested/commonFunctions.py
+++ EclipseShitzProjects/CommonFunctions/src/root/nested/commonFunctions.py
@@ -36,4 +36,4 @@ def chanceOfHappen(a, b):
 def percentChange(a, b): #percentage increase or decrease from a to b
     changy=Decimal(b)/Decimal(a)
     return (changy-1)*100
-#assholes and titties
+#assholes and titties OSISISdi
