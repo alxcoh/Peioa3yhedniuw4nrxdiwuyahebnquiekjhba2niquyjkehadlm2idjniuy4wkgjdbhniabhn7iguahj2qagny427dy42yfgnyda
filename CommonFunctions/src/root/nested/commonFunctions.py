d55af02dad750ffdf6d711fdf4033d23bf212d05
71619997a <marksg07@gmail.com>
2013-12-22 14:56:11 -0500

    add comments :)
    lol

--- CommonFunctions/src/root/nested/commonFunctions.py
+++ CommonFunctions/src/root/nested/commonFunctions.py
@@ -5,7 +5,7 @@ Created on Dec 22, 2013
 '''
 
 def dimensionList(values):
-    a=[0 for i in range(values[0])]
-    for i in range(1, len(values)):
-        a=[a for i in range(values[i])]
-    return a
+    a=[0 for i in range(values[0])] #first, init base array
+    for i in range(1, len(values)): #for number of dimensions:
+        a=[a for i in range(values[i])] #the array = the old array but length # of times
+    return a #smartness, nice one
