ae0e6c5f21f80b7156007cad584f6328bc4c4135
71619997a <marksg07@gmail.com>
2013-12-22 15:00:05 -0500

    add print2DArray
    prints 2d arrays in multiple rows instead of just one straight line

--- CommonFunctions/src/root/nested/commonFunctions.py
+++ CommonFunctions/src/root/nested/commonFunctions.py
@@ -9,3 +9,8 @@ def dimensionList(values):
     for i in range(1, len(values)): #for number of dimensions:
         a=[a for i in range(values[i])] #the array = the old array but length # of times
     return a #smartness, nice one
+
+def print2DArray(array): #a readable print of a 2d array, actually in 2 dimensions
+    for i in range(len(array)):
+        print array[i]
+    
