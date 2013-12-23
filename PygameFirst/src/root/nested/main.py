33af832e1ede5a8b9fbd3347435727fb4fd25132
alxcoh <alxcoh@gmail.com>
2013-12-22 21:10:21 -0500

    pygame shitsky

--- PygameFirst/src/root/nested/main.py
+++ PygameFirst/src/root/nested/main.py
@@ -3,3 +3,16 @@ Created on Dec 22, 2013
 
 @author: alxcoh
 '''
+
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Hello World!')
+while True: # main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
