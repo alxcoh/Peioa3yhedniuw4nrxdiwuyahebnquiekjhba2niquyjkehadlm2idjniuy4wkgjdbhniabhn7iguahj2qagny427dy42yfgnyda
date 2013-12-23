c5af951d647ebe0fd084cc659277b29cb537f789
alxcoh <alxcoh@gmail.com>
2013-12-23 12:20:59 -0500

    more organized (functions for stuff)

--- BouncyBall/src/root/nested/main.py
+++ BouncyBall/src/root/nested/main.py
@@ -8,8 +8,28 @@ from pygame.locals import *
 #Colors
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
-
-
+yPos=450
+xPos=400
+goingDown=1
+goingRight=1
+def ballMove():
+    if goingDown==1:
+        yPos+=8
+    if goingDown==0:
+        yPos-=8
+    if goingRight==1:
+        xPos+=8
+    if goingRight==0:
+        xPos-=8
+def ballCheck():
+    if yPos>=760:
+        goingDown=0
+    elif yPos<=40:
+        goingDown=1
+    if xPos>=860:
+        goingRight=0
+    elif xPos<=40:
+        goingRight=1
 def main():
     #Initialization
     pygame.init()
@@ -21,10 +41,7 @@ def main():
     background = background.convert()
     background.fill(WHITE)
     
-    yPos=450
-    xPos=400
-    goingDown=1
-    goingRight=1
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -33,22 +50,8 @@ def main():
         background.fill(WHITE)
         myBallCenterPos = (xPos, yPos)
         pygame.draw.circle(background, BLACK, myBallCenterPos, 40)
-        if yPos>=760:
-            goingDown=0
-        elif yPos<=40:
-            goingDown=1
-        if xPos>=860:
-            goingRight=0
-        elif xPos<=40:
-            goingRight=1
-        if goingDown==1:
-            yPos+=8
-        if goingDown==0:
-            yPos-=8
-        if goingRight==1:
-            xPos+=8
-        if goingRight==0:
-            xPos-=8
+        ballCheck()
+        ballMove()
         pygame.display.flip()
         pygame.display.update()
 main()
