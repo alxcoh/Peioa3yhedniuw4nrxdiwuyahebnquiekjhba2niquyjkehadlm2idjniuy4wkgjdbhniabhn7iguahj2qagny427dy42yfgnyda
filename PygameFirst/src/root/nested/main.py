b6a8a473c091373f6630c21261e70083e256fecd
71619997a <marksg07@gmail.com>
2013-12-22 22:35:09 -0500

    shitty f
    uk

--- PygameFirst/src/root/nested/main.py
+++ PygameFirst/src/root/nested/main.py
@@ -12,16 +12,30 @@ def main():
     # Initialise screen
     pygame.init()
     screen = pygame.display.set_mode((900, 800))
-    pygame.display.set_caption('Basic Pygame program')
+    pygame.display.set_caption('sqiggity sqag')
 
     # Fill background
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill((255, 255, 255))
-
+    topR=252
+    lowR=3
+    topB=252
+    lowB=3
+    topG=252
+    lowG=3
+    rUp=True
+    gUp=False
+    bUp=True
+    rIter=4
+    bIter=2
+    gIter=3
+    r=89
+    g=89
+    b=89
+    background.fill((r, g, b))
     # Display some text
     font = pygame.font.Font(None, 36)
-    text = font.render("Hello There", 1, (10, 10, 10))
+    text = font.render("Invisible Penis Unicorn HA I AM SO SMART", 1, (10, 10, 10))
     textpos = text.get_rect()
     textpos.centerx = background.get_rect().centerx
     background.blit(text, textpos)
@@ -30,13 +44,33 @@ def main():
     screen.blit(background, (0, 0))
     pygame.display.flip()
 
-    
     while 1: # Event loop
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
-
+        if r<=lowR or r>=topR:
+            rUp=not rUp
+        if g<=lowG or g>=topG:
+            gUp=not gUp
+        if b<=lowB or b>=topB:
+            bUp=not bUp
+        if rUp:
+            r+=rIter
+        else:
+            r-=rIter
+        if gUp:
+            g+=gIter
+        else:
+            g-=gIter
+        if bUp:
+            b+=bIter
+        else:
+            b-=bIter
+            
+            
+        background.fill((r, g, b))
         screen.blit(background, (0, 0))
+        screen.blit(text, textpos)
         pygame.display.flip()
 
 
