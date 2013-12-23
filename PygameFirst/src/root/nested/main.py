3b9dbfd2372c53ce2d7ae87da8c2c1a3e8663969
alxcoh <alxcoh@gmail.com>
2013-12-22 22:00:49 -0500

    interwebs version

--- PygameFirst/src/root/nested/main.py
+++ PygameFirst/src/root/nested/main.py
@@ -8,12 +8,36 @@ import pygame, sys
 #from pygame import *
 from pygame.locals import *
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World!')
-while True: # main game loop
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+def main():
+    # Initialise screen
+    pygame.init()
+    screen = pygame.display.set_mode((900, 800))
+    pygame.display.set_caption('Basic Pygame program')
+
+    # Fill background
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((255, 255, 255))
+
+    # Display some text
+    font = pygame.font.Font(None, 36)
+    text = font.render("Hello There", 1, (10, 10, 10))
+    textpos = text.get_rect()
+    textpos.centerx = background.get_rect().centerx
+    background.blit(text, textpos)
+
+    # Blit everything to the screen
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+
+    
+    while 1: # Event loop
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
+
+
+main()
