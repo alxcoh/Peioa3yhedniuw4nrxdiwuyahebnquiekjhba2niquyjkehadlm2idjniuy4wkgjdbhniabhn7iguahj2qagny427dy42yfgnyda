7ca9fb32234ff75f4145e6e39edf7a68c404893d
71619997a <marksg07@gmail.com>
2013-12-25 14:54:21 -0500

    remove trans

--- BouncyBall/src/root/nested/main.py
+++ BouncyBall/src/root/nested/main.py
@@ -81,7 +81,7 @@ textpos = pauseText.get_rect()
 textpos.centerx = background.get_rect().centerx
 textpos.centery = background.get_rect().centery
 s = pygame.Surface((1000,750))  # the size of your rect
-s.set_alpha(6)                # alpha level
+s.set_alpha(2)                # alpha level
 s.fill((255,255,255))           # this fills the entire surface
 
 while not end:
@@ -122,7 +122,6 @@ while not end:
                 break
             if event.key == K_p: 
                 pause=not pause     # pause or unpause
-                trans=not trans
 
 
 ################################### C H A T ###################################
