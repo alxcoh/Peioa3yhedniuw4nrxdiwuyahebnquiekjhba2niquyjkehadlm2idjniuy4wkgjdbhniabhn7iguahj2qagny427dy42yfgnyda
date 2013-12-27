a97300250cfe6d34d6dd9ae7bec7b1631094bba6
alxcoh <alxcoh@gmail.com>
2013-12-27 02:32:08 -0500

    mostlyworks

--- BouncyBall/src/root/nested/main.py
+++ BouncyBall/src/root/nested/main.py
@@ -36,7 +36,7 @@ paddleHeight=[150 for i in range(2)]
 
 paddleY=[350-(paddleHeight[0]/2), 350-(paddleHeight[1]/2)] # 0 is left, 1 is right
 
-paddleSpeed=[10, 10]
+paddleSpeed=[10, 4]
 
 paddleLeft=pygame.Rect(15, paddleY[0], 15, paddleHeight[0])
 paddleRight=pygame.Rect(970, paddleY[1], 15, paddleHeight[1])
@@ -106,8 +106,8 @@ def ballCheck(a, b, c, d):
         print 'Real: ', yPos
         print 'Difference: ', val-yPos
         scoreL+=1
-        xPos=450
-        yPos=400
+        xPos=500
+        yPos=350
         paddleY[1]=275
         paddleY[0]=275
         rVal=random.randrange(-2, 2)
@@ -126,8 +126,8 @@ def ballCheck(a, b, c, d):
         pause=True
         
     if xPos<=30:
-        xPos=450
-        yPos=400
+        xPos=500
+        yPos=350
         paddleY[0]=275
         paddleY[1]=275
         '''goingRight=randomizeMovement(0,startspeedR*1.5)
