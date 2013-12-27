fae7f34380eb1e1b275d9ccf8b132644bea68a29
alxcoh <alxcoh@gmail.com>
2013-12-26 19:15:29 -0500

    these is the sim

--- BouncyBall/src/root/nested/simTest.py
+++ BouncyBall/src/root/nested/simTest.py
@@ -3,3 +3,24 @@ Created on Dec 26, 2013
 
 @author: alxcoh
 '''
+def simTester(side, xPosy, yPosy, xVel, yVel): #0 is left 1 is right
+    hit=False #if the sim reaches its end
+    side=side
+    xPosy=xPosy
+    yPosy=yPosy
+    xVel=xVel
+    yVel=yVel
+    if side==0:
+        while hit==False:
+            if xPosy<=30:
+                return yPosy
+            else:
+                xPosy+=xVel
+                yPosy+=yVel
+    if side==1:
+        while hit==False:
+            if xPosy>=970:
+                    return yPosy
+            else:
+                xPosy+=xVel
+                yPosy+=yVel
